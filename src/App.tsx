188b103c5939837af51ba6d35455180a072f1142
Kartik Kant <kartik@Kartiks-MacBook-Air.local>
2025-08-31 23:06:18 +0530

    Add Meta Pixel to HTML head section as requested by Facebook

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MetaPixel } from "./components/MetaPixel";
 import Index from "./pages/index";
 import Checkout from "./pages/Checkout";
 import Success from "./pages/Success";
@@ -21,7 +20,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <MetaPixel pixelId="3624737897834830" />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
